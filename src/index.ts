d4567bfae75050665d1d5234a27e81b03e66681b
zarifhoque <hoquelabid@gmail.com>
2025-11-17 16:39:50 +0600

    feat: set up basic Express server for Issue#1

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+
+const PORT: number = parseInt(process.env.PORT || '3000', 10);
+if (isNaN(PORT)) {
+    throw new Error('Invalid PORT number');
+}
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response): void => {
+  res.status(200).send('Movie Review API is running!');
+});
+
+app.listen(PORT, (): void => {
+  console.log(`Server running on port ${PORT}`);
+}).on('error', (err: Error) => {
+    console.error('Server error:', err)
+});
