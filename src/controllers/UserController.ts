6321effcb859d2fd919b271f88216ac24abd6070
zarifhoque <hoquelabid@gmail.com>
2025-11-26 10:38:05 +0600

    feat: schema validation for query params

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { UserService } from '../services/UserService';
-import { UserCreateDTO, UserResponseDTO, UserUpdateDTO } from '../dtos/UserDTOs';
+import { UserCreateDTO, UserUpdateDTO } from '../dtos/UserDTOs';
 import { z } from 'zod';
 
 const userService = new UserService();
