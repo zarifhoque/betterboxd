472ea19fa4118f6f39efda7cb3cb53f618ba3872
zarifhoque <hoquelabid@gmail.com>
2025-11-26 15:48:30 +0600

    fix: handle both query params and body with one function

--- src/middlewares/ValidationHandler.ts
+++ src/middlewares/ValidationHandler.ts
@@ -1,12 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
-import { ZodObject } from 'zod';
+import { ZodObject, ZodTypeAny } from 'zod';
 
-export function validationHandler(schema: ZodObject) {
+interface ValidationOptions {
+  source?: 'body' | 'query' | 'params';
+}
+
+export function validationHandler<T extends ZodTypeAny>(
+  schema: ZodObject<T>,
+  options: ValidationOptions = { source: 'body' },
+) {
   return (req: Request, _res: Response, next: NextFunction) => {
-    const result: ReturnType<typeof schema.safeParse> = schema.safeParse(req.body);
+    const source = options.source || 'body';
+    const data = req[source];
+    const result: ReturnType<typeof schema.safeParse> = schema.safeParse(data);
     if (!result.success) {
       return next(result.error);
     }
+    req[source] = result.data;
     next();
   };
 }
