a55d5336ccc67cf15e3c76c13ef781dbf521a0f3
zarifhoque <hoquelabid@gmail.com>
2025-11-26 09:34:25 +0600

    fix: remove unnecessary typing

--- src/controllers/StoryController.ts
+++ src/controllers/StoryController.ts
@@ -1,7 +1,7 @@
 // controllers/storyController.ts
 import { Request, Response, NextFunction } from 'express';
 import { StoryService } from '../services/StoryService';
-import { StoryResponseDTO, StoryUpdateDTO } from '../dtos/StoryDTOs';
+import { StoryUpdateDTO } from '../dtos/StoryDTOs';
 import z from 'zod';
 
 const storyService = new StoryService();
@@ -9,7 +9,7 @@ const storyService = new StoryService();
 export class StoryController {
   static async getAllStories(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      const stories: StoryResponseDTO[] = await storyService.getAllStories();
+      const stories = await storyService.getAllStories();
       res.json({ success: true, data: stories, message: 'Stories fetched successfully' });
     } catch (error: unknown) {
       next(error);
@@ -29,7 +29,7 @@ export class StoryController {
 
   static async createStory(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      const story: StoryResponseDTO = await storyService.createStory(req.body);
+      const story = await storyService.createStory(req.body);
       res.status(201).json({ success: true, data: story, message: 'Story created successfully' });
     } catch (error) {
       next(error);
